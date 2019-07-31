735f29ce463a9f766fdf9343fe35610b5d6e0f94
eggogg <eggogg@gmail.com>
2019-07-31 17:33:18 +0400

    Correct some errors

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,7 +2,6 @@
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .login_page import LoginPage
-from .pages.login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -13,10 +12,3 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def test_guest_can_go_to_login_page(browser):
-        link = "http://selenium1py.pythonanywhere.com"
-        page = MainPage(browser, link)
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page()

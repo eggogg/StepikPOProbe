5151fd72a46e43ffffd298e6e55003b589ea4085
eggogg <eggogg@gmail.com>
2019-07-31 16:46:19 +0400

    Add login_page

---
+++ pages/login_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert browser.current_url.find("login") == -1, "Login url is not corrected"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Registration form is not presented"

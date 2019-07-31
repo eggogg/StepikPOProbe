dfacc9a3a5d91d37b0483cbd49b6ec18db627740
eggogg <eggogg@gmail.com>
2019-07-31 15:28:00 +0400

    Improve should_be_login_link and some other changes

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
-﻿class BasePage(object):
+﻿from selenium.common.exceptions import NoSuchElementException
+
+class BasePage(object):
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
@@ -8,6 +10,6 @@
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except ():
+        except (NoSuchElementException):
             return False
         return True

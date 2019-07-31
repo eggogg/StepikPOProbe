730faecb773c68b630015ffba340e08bacad7c98
Igor Yu.Saltykov <eggogg@gmail.com>
2019-07-31 20:59:42 +0400

    Correct bug with login in url

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,9 @@
-﻿from .pages.main_page import MainPage
+﻿#import pytest
+from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def go_to_login_page(browser):
     link = browser.find_element_by_css_selector("#login_link")
-#    link = browser.find_element_by_css_selector("#registration_link")
     link.click()
 
 def test_guest_can_go_to_login_page(browser):
@@ -14,6 +14,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+#@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
